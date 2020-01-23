1b21eca29936483a15d9ebc18f63fd47644c5f8a
goblincore <donny@Donnys-MacBook-Pro.local>
2020-01-23 13:12:59 -0500

    Add additional class properties and methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  includeLetters=false;
+  includeNumbers=false;
+  includeSymbols=false;
   password='';
 
   onButtonClick(){
+    console.log(this.includeLetters);
     this.password='my password';
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = ! this.includeSymbols;
   }
 
 }
